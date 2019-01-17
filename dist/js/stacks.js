b5f0592c2f1f1b0af0c6301a29d9c39a19e54db3
Benjamin Dumke-von der Ehe <mail@benjamin-dumke.de>
2019-01-17 16:17:29 +0100

    s-revealer and s-popover JS experimentation

--- dist/js/stacks.js
+++ dist/js/stacks.js
@@ -1822,6 +1822,82 @@ Copyright © 2018 Basecamp, LLC
 
 (function () {
     "use strict";
+
+    Stacks.addController("s-popover", {
+        show: function () {
+            this.element.setAttribute("aria-hidden", "false");
+        },
+        hide: function () {
+            this.element.setAttribute("aria-hidden", "true");
+        },
+        toggle: function () {
+            this.element.setAttribute("aria-hidden", this.element.getAttribute("aria-hidden") === "false" ? "true" : "false");
+        }
+    });
+
+})();
+
+
+
+;
+
+(function () {
+    "use strict";
+
+    var revealableControllers = ["s-popover"];
+
+    Stacks.addController("s-revealer", {
+
+        forEachTarget: function (f) {
+            document.querySelectorAll(this.data.get("target")).forEach(f);
+        },
+
+        getRevealableController: function (el) {
+            var result = null;
+            for (var i = 0; i < revealableControllers.length; i++) {
+                result = this.application.getControllerForElementAndIdentifier(el, revealableControllers[i]);
+                if (result) {
+                    break;
+                }
+            }
+            return result;
+        },
+
+        callRemote: function(methodName) {
+            var that = this;
+            this.forEachTarget(function (e) {
+                var c = that.getRevealableController(e);
+                if (c) {
+                    c[methodName]();
+                }
+            });
+        },
+
+        show: function(evt) {
+            evt.preventDefault();
+            this.callRemote("show");
+        },
+
+        hide: function(evt) {
+            evt.preventDefault();
+            this.callRemote("hide");
+        },
+
+        toggle: function(evt) {
+            evt.preventDefault();
+            this.callRemote("toggle");
+        },
+
+
+    });
+
+})();
+
+
+;
+
+(function () {
+    "use strict";
     Stacks.addController("s-table", {
 
         targets: ["column"],
